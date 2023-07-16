420e49485418ad8a9b45893d6d8ecb5ee433b56f
Charlie Fadness <fadness.charlie@outlook.com>
2023-07-16 03:29:20 +0000

    changed banner time

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import PackageSale from './sale/PackageSale';
 
 /* Year Month Day TIME(T) Hour Minute Seconds*/
 let setStartDate = new Date('2023-05-01T00:00:01');
-let setEndDate = new Date('2023-09-13T23:59:00');
+let setEndDate = new Date('2023-06-13T23:59:00');
 
 let startDate = setStartDate.getTime();
 let endDate = setEndDate.getTime();
